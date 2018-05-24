1fb10192885223b07a8f191078e74af8a1f8426e
serenaliaojc <36046972+serenaliaojc@users.noreply.github.com>
2018-05-24 10:43:13 -0400

    lab3-gmap part2-test

--- lab3-gmap/script.ts
+++ lab3-gmap/script.ts
@@ -66,12 +66,10 @@ function initMap() {
 		});
 		// console.log(marker);
 		// marker.setMap(map);
-		google.maps.event.addListener(marker, 'click', (function(marker, i) {
-			return function() {
-				infowindow.setContent(locations[i][0]);
-				infowindow.open(map, marker);
-			}
-		})(marker, i));
+	}
+
+	function addMarker():void {
+		// body...
 	}
 
 	function getLatLng(address: string): LatLng {
